f615e8031a30728943c6831e7bf09607ef60158e
Carlos Castilla <carlos.castilla009@gmail.com>
2024-01-08 10:10:33 -0500

    feat: add crashlytics

--- App.tsx
+++ App.tsx
@@ -5,114 +5,56 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {useEffect} from 'react';
+import {Button, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import crashlytics from '@react-native-firebase/crashlytics';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+async function onSignIn(user: any) {
+  try {
+    crashlytics().log('User signed in.');
+    await Promise.all([
+      crashlytics().setUserId(user.uid),
+      crashlytics().setAttribute('credits', String(user.credits)),
+      crashlytics().setAttributes({
+        role: 'admin',
+        followers: '13',
+        email: user.email,
+        username: user.username,
+      }),
+    ]);
+    // crashlytics().recordError(new Error('hola como estas?'));
+    crashlytics().recordError(new Error('Estoy bien y tu?'));
+    // crashlytics().crash();
+    console.log('data registrada');
+  } catch (error) {
+    console.log('🚀 -----------------------------------------------🚀');
+    console.log('🚀 ~ file: App.tsx:28 ~ onSignIn ~ error:', error);
+    console.log('🚀 -----------------------------------------------🚀');
+  }
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    crashlytics().log('App mounted.');
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View>
+      <Button
+        title="Sign In"
+        onPress={() =>
+          onSignIn({
+            uid: 'Aa0Bb1Cc2Dd3Ee4Ff5Gg6Hh7Ii8Jj9',
+            username: 'Joaquin Phoenix',
+            email: 'phoenix@example.com',
+            credits: 42,
+          })
+        }
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Button title="Test Crash" onPress={() => crashlytics().crash()} />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
